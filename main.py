bceb17a8d37f9f6723e9259a3d65c09d1891a7a7
Andres Abdala Alvarez <andrespampa@gmail.com>
2025-09-20 22:03:15 -0300

    Empezando con JWT, seccion 4 autenticacion, Generador de Token - Video 19

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from fastapi import FastAPI, Body
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, Body, Path, Query, Response
+from fastapi.responses import HTMLResponse, JSONResponse, RedirectResponse
 from typing import Dict, List, Any, Optional
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 app = FastAPI(
     title="Mi primer api con fastapi",
@@ -9,11 +9,10 @@ app = FastAPI(
     version="0.0.1",
 )
 
-@app.get('/', tags=["Inicio"])#el argumento del decorador nos pide como se va a llamar la ruta
+@app.get('/', tags=["Inicio"]) #el argumento del decorador nos pide como se va a llamar la ruta
 #Primer Approach
 # def read_root() -> Dict[str, str]:
 #     return {"Hello": "world"}
-
 #Segundo Approach
 def read_root() -> HTMLResponse:
     return HTMLResponse('<h2>Hola Mundo!</h2>')
@@ -21,12 +20,21 @@ def read_root() -> HTMLResponse:
 #Clase de python que hereda de BaseModel
 class Movie(BaseModel):
     id: Optional[int] = None
-    title: str
-    overview: str
-    year: int
-    rating: float
-    category: str
+    title: str = Field(default='Titulo de la pelicula', min_length=5, max_length=60)
+    overview: str = Field(default='Descripcion de la pelicula', min_length=15, max_length=60)
+    year: int = Field(default=2023)
+    rating: float = Field(ge=1, le=10)#ge = greater than or equal (mayor o igual que) le = less than or equal (menor o igual que)
+    category: str = Field(default='Categoria de la pelicula', min_length=3, max_length=20)
 
+    # def to_dict(self):
+    #     return {
+    #         'id':self.id,
+    #         'title':self.title,
+    #         'overview': self.overview,
+    #         'year': self.year,
+    #         'rating': self.rating,
+    #         'category': self.category
+    #     }
 
 
 
@@ -43,11 +51,11 @@ movies = [
 ]
 
 @app.get('/movies', tags=["Get Movies"])
-def get_movies() -> List[Dict]:
-    return movies
+def get_movies() -> JSONResponse: #-> List[Dict]:
+    return JSONResponse(content=movies)
 
 @app.get('/movies/{id}', tags=["Get Movies"]) #tags=["Get Movie By ID"]) #Ponemos el parametros dentro de {}
-def get_movie_by_id(id: int) -> Dict:
+def get_movie_by_id(id: int = Path(ge=1, le=100)) -> Dict:
     for item in movies:
         if item['id'] == id:
             return item
@@ -56,7 +64,8 @@ def get_movie_by_id(id: int) -> Dict:
 
 
 @app.get('/movies/', tags=["Get Movies"])
-def get_movies_by_category(category: str):# -> List[Dict]:
+def get_movies_by_category(category: str = Query(min_length=3, max_length=20)) -> str:
+
     return category
 
 # #Endpoint sin pydantic
@@ -76,12 +85,12 @@ def get_movies_by_category(category: str):# -> List[Dict]:
 #     return title
 
 #Endpoint sin pydantic
-@app.post('/movies', tags=["Movies"])
-def create_movie(movie: Movie) -> Movie:
+@app.post('/movies', tags=["Movies"], status_code=201)
+def create_movie(movie: Movie) -> JSONResponse: #-> Movie:
     movies.append(movie)
     print(movies)
     # return movie.title
-    return movie
+    return JSONResponse(status_code=201 ,content={"message": "Se ha cargado una nueva pelicula correctamente", "movies": movie.dict()})
 
 # #Endpoint sin pydantic
 # @app.put('/movies/{id}',tags=["Movies"])
@@ -105,7 +114,7 @@ def create_movie(movie: Movie) -> Movie:
 #         return None
     
 #Endpoint con pydantic
-@app.put('/movies/{id}',tags=["Movies"])
+@app.put('/movies/{id}',tags=["Movies"], status_code=200)
 def update_movie(id:int, movie:Movie) -> List[Dict[str, Any]] | None: # o tambien ===> Union[List[Dict[str, Any]], None]
     for item in movies:
         if item["id"] == id:
@@ -114,15 +123,16 @@ def update_movie(id:int, movie:Movie) -> List[Dict[str, Any]] | None: # o tambie
             item["year"] = movie.year
             item["rating"] = movie.rating
             item["category"] = movie.category
-            return movies
+            return JSONResponse(content={"message": "Se ha actualizado la pelicula correctamente", "movies": movie.to_dict()})
         
         print("No se ha encontrado la pelicula")
         return None
 
-@app.delete('/movies/{id}', tags=["Movies"])
-def delete_movies(id: int) -> List[Dict[str, Any]] | None:
+@app.delete('/movies/{id}', tags=["Movies"], status_code=200)
+def delete_movies(id: int) -> JSONResponse: #-> List[Dict[str, Any]] | None:
     # print(movies) #como no hay persistencia de datos no hay drama en eliminar la pelicula
     for item in movies:
         if item['id'] == id:
             movies.remove(item)
-            return movies
+            return JSONResponse(content={"message": "Se ha Eliminado la pelicula correctamente", "movies": movies})
+        
